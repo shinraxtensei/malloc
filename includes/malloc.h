b9a12c00f5ca1f52d58edcc2c73672679a938e73
anasshouarii@gmail.com <anasshouarii@gmail.com>
2025-07-06 09:43:17 +0100

    Fix author name and formatting in malloc.h

--- includes/malloc.h
+++ includes/malloc.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   malloc.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anasshouari <anasshouari@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ahouari <ahouari@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/05 00:00:00 by ashouari          #+#    #+#             */
-/*   Updated: 2025/07/05 18:59:11 by anasshouari      ###   ########.fr       */
+/*   Created: 2025/01/05 00:00:00 by ahouari          #+#    #+#             */
+/*   Updated: 2025/07/05 18:59:11 by ahouari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@
 # define MIN_ALLOCS_PER_ZONE 100
 # define TINY_ZONE_ALLOCS 100
 # define SMALL_ZONE_ALLOCS 100
-
-
 
 typedef enum e_zone_type
 {
@@ -66,19 +64,19 @@ typedef struct s_memory
 extern t_memory			g_memory;
 extern pthread_mutex_t	g_malloc_mutex;
 
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t		ft_strlen(const char *s);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
 
-void	free(void *ptr);
-void	*malloc(size_t size);
-void	*realloc(void *ptr, size_t size);
-void	show_alloc_mem(void);
-void	show_alloc_mem_ex(void);
-void	print_zone_blocks(t_zone *zone);
-void	init_memory_manager(void);
-void	*allocate_memory(size_t aligned_size);
-void	free_memory(void *ptr);
+void		free(void *ptr);
+void		*malloc(size_t size);
+void		*realloc(void *ptr, size_t size);
+void		show_alloc_mem(void);
+void		show_alloc_mem_ex(void);
+void		print_zone_blocks(t_zone *zone);
+void		init_memory_manager(void);
+void		*allocate_memory(size_t aligned_size);
+void		free_memory(void *ptr);
 
 t_zone		*create_zone(size_t size, t_zone_type type);
 void		add_zone(t_zone *zone, t_zone_type type);
@@ -93,7 +91,5 @@ size_t		calculate_total_memory(void);
 
 size_t		align_size(size_t size);
 size_t		get_page_size(void);
-
-extern t_memory	g_memory;
 
 #endif
